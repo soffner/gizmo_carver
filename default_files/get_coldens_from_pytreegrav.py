ff985cda6ddb3c3d5bc18ac8d8ef110fdc94915d
psharda <piyush.suneet@gmail.com>
2024-01-05 12:00:19 +0100

    added proper naming for file to be saved

--- default_files/get_coldens_from_pytreegrav.py
+++ default_files/get_coldens_from_pytreegrav.py
@@ -1,8 +1,12 @@
 import h5py
 from meshoid import Meshoid
 import pytreegrav
+import sys
+sys.path.append('/g/data1b/jh2/ps3459/gizmo_carver/src/')  # Add the directory to sys.path
+import inputs_gizmo_carver as inputs
+import numpy as np
 
-F = h5py.File('snapshot_000.hdf5', 'r')
+F = h5py.File(inputs.hdf5_dir + 'snapshot_'+inputs.snap+'.hdf5', 'r')
 
 rho = F["PartType0"]["Density"][:]
 pdata = {}
@@ -19,10 +23,11 @@ l_eff = (3 * vol / (4 * 3.14159))**(1./3.) #we model each gas particle as a sphe
 kappa = 0.02 #opacity in code units. Accurate opacity really matters only if the column is very anisotropic.
 sigma = mass*kappa
 
-bb = pytreegrav.ColumnDensity(pos, sigma, l_eff, parallel=True, randomize_rays=True, rays=100)
+rays = 10
+bb = pytreegrav.ColumnDensity(pos, sigma, l_eff, parallel=True, randomize_rays=True, rays=rays)
 bb_eff = -np.log(np.exp(-bb.clip(-300,300)).mean(axis=1)) # effective optical depth that would give the same radiation flux from a background; note clipping because overflow is not uncommon here
 NH_eff = bb_eff / kappa # effective column density *for this opacity* in code mass/code length^2
 
 
 #save the column densities in a txt file
-np.savetxt('nh_eff_100rays.txt', NH_eff, fmt='%e')
+np.savetxt(inputs.hdf5_dir+'nh_eff_meshoid_'+str(rays)+'rays_'+inputs.snap+'.txt', NH_eff, fmt='%e')
