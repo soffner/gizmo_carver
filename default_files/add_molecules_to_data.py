ca18743fb0e58d80c47c2f51bcfdb9ead0b77477
psharda <piyush.suneet@gmail.com>
2024-01-28 10:26:24 +0100

    calculate and store number densities

--- default_files/add_molecules_to_data.py
+++ default_files/add_molecules_to_data.py
@@ -13,6 +13,10 @@ f = h5py.File(inputs.hdf5_dir + 'snapshot_'+inputs.snap+'.hdf5', 'a')
 print('Opened file ', f)
 
 #delete existing datasets of the same name:
+if 'NumDensity' in f['/PartType0']:
+	del f['/PartType0/NumDensity']
+	print('Deleted existing dataset NumDensity')
+
 if 'HColDensity' in f['/PartType0']:
 	del f['/PartType0/HColDensity']
 	print('Deleted existing dataset HColDensity')
@@ -49,6 +53,12 @@ if 'HNCAbundUCLCHEM' in f['/PartType0']:
            del f['/PartType0/HNCAbundUCLCHEM']
            print('Deleted existing dataset HNCAbundUCLCHEM')
 
+#we use the following unit base for starforge simulations
+#unit_base = {'UnitMagneticField_in_gauss':  1e+4,
+#                 'UnitLength_in_cm'         : cons.pc.cgs.value,
+#                 'UnitMass_in_g'            : cons.M_sun.cgs.value,
+#                 'UnitVelocity_in_cm_per_s' :      100}
+
 nh_eff = np.loadtxt(inputs.hdf5_dir+'nh_eff_meshoid_10rays_'+inputs.snap+'.txt')
 xH2 = f['PartType0']['MolecularMassFraction'][:]*f['PartType0']['NeutralHydrogenAbundance'][:]/inputs.mol_hydrogen_ratio
 mean_mass_H = 1.0 + inputs.helium_mass_fraction
@@ -57,6 +67,10 @@ mH = cons.m_p.cgs.value + cons.m_e.cgs.value
 nh2 = nh_eff * ((cons.M_sun.cgs.value/cons.pc.cgs.value**2)/(mH*mean_mol_mass)) #in cm**-2; in new_fields_yt.py, we will create field H2ColumnDensity from this field (so the units are consistent)
 f.create_dataset('/PartType0/H2ColDensity', data=nh2)
 print('Created new dataset H2ColDensity')
+
+nh = f['PartType0']['Density'][:]*(cons.M_sun.cgs.value/cons.pc.cgs.value**3)/(mH*mean_mol_mass) #in cm**-3; in new_fiels_yt.py we will add units
+f.create_dataset('/PartType0/NumDensity', data=nh)
+print('Created new dataset NumDensity')
 
 co_desp = np.loadtxt(inputs.hdf5_dir+'co_abund_despotic_'+inputs.snap+'.txt')
 f.create_dataset('/PartType0/COAbundDespotic', data=co_desp)
